0718a92099abfbccc65720d0f9072dcf28d0c8a7
ayukawai <160094689+ayukawai@users.noreply.github.com>
2024-02-15 17:29:58 +0900

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,30 +13,74 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+  const books = /* retrieve books */;
+  
+  // Use JSON.stringify to display the output neatly
+  const booksJson = JSON.stringify(books, null, 2);
+
+  // Send the response with the list of books
+  res.send(booksJson);
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
+  const isbn = req.params.isbn;
+  
+  // Find the book details based on ISBN from your data source
+  const book = /* retrieve book details by ISBN */;
+  
+  // Send the response with book details
+  res.send(book);
+  
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+  const author = req.params.author;
+  
+  // Find books by the specified author from your data source
+  const booksByAuthor = /* retrieve books by author */;
+  
+  // Send the response with books by the specified author
+  res.send(booksByAuthor);
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
+  const title = req.params.title;
+  
+  // Find books by the specified title from your data source
+  const booksByTitle = /* retrieve books by title */;
+  
+  // Send the response with books by the specified title
+  res.send(booksByTitle);
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
+  const isbn = req.params.isbn;
+
+  // Assuming you have a data structure containing book reviews
+  const bookReviews = getBookReviewsByISBN(isbn);
+
+  // Check if there are reviews for the book
+  if (bookReviews) {
+    res.send(bookReviews);
+  } else {
+    res.status(404).send({ error: 'No reviews found for the given ISBN.' });
+  }
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
